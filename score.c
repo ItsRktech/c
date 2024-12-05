392231970f5120624460a08a4e4a8b1d7842c91d
ItsRktech <karnriki70@gmail.com>
2024-12-05 17:03:11 +0545

    nxt

--- score.c
+++ score.c
@@ -6,9 +6,9 @@ void main()
     int rollno;
     float msub1, msub2, msub3, msub4, msub5, msum, score;
     printf("Enter the roll number of student:");
-    scanf("%d",rollno);
+    scanf("%d",&rollno);
     printf("Enter the marks of 5 subjects:");
-    scanf("%f%f%f%f%f",msub1, msub2, msub3, msub4, msub5);
+    scanf("%f%f%f%f%f",&msub1, &msub2, &msub3, &msub4, &msub5);
     msum = msub1 + msub2 + msub3 + msub4 + msub5;
     score = (msum/500)*100;
     printf("The student's roll number is %d and score is %f%.",rollno, score);
