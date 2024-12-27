6da13366ea15da71010cb1bc7e83f0b4b6f0f9e7
RK <karnriki70@gmail.com>
2024-12-27 21:13:04 +0545

    Bonus of employee.

---
+++ bonus.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char gender;
+    float sales,bonus;
+    printf("Enter the gender of employee(M/F):\n");
+    scanf("%c",&gender);
+    printf("Enter the sales of employee:\n");
+    scanf("%f,",&sales);
+    if(gender=='M')
+    {
+        if(sales<5000)
+        {
+            bonus=0.05*sales;
+        }
+        else
+        {
+            bonus=0.10*sales;
+        }
+        printf("The bonus of employee is %.2f",bonus);
+    }
+    else if(gender=='F')
+    {
+        if(sales<5000)
+        {
+            bonus=0.07*sales;
+        }
+        else
+        {
+            bonus=0.12*sales;
+        }
+        printf("The bonus of employee is %.2f",bonus);
+    }
+    else
+    {
+        printf("Invalid gender.\n");
+    }
+    getch();
+    return 0;
+}
